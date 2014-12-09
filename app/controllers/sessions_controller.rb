8f0b3ff5678c3147c3c3147acffd9a7fefcd0323
Mikhail Mandronov <m.mandronov@ltst.ru>
2014-12-09 23:41:01 +0600

    edit auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   # before_action :authenticate, only: :destroy
   def create
     auth_hash = request.env['omniauth.auth']
-    user = User.find_or_create_by(uid: @auth_hash['uid'], provider: auth_hash['provider'])
+    user = User.find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
 
     if user
       redirect_to :gem_notes
