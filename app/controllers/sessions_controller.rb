470a27eedd0cb1b3fb77ebe637e2a2703af1e459
Mikhail Mandronov <m.mandronov@ltst.ru>
2014-12-09 22:54:02 +0600

    edit auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,14 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: [:create, :failure]
   # before_action :authenticate, only: :destroy
   def create
+    auth_hash = request.env['omniauth.auth']
+    user = User.find_or_create_by(uid: @auth_hash['uid'], provider: auth_hash['provider'])
 
-    @auth_hash = request.env['omniauth.auth']
-    # logger.info auth_hash
-    # user = User.find_or_create_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
-
-    # if user
-    #   redirect_to :gem_notes
-    # else
-    #   render 'home/index'
-    # end
-    render 'sessions/success'
+    if user
+      redirect_to :gem_notes
+    else
+      render 'home/index'
+    end
   end
 
   def failure
